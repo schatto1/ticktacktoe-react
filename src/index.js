3663d67b081933fe33603ce29c6bdbf606d4eb48
Sumon Chattopadhyay <s.chattopadhyay89@gmail.com>
2021-11-16 14:53:04 -0800

    Changes Board to store state of game

--- src/index.js
+++ src/index.js
@@ -14,9 +14,7 @@ class Square extends React.Component {
     return (
       <button
         className="square"
-        onClick={function () {
-          this.setState({ value: 'X' });
-        }}
+        onClick={() => this.setState({ value: 'X' })}
       >
         {this.state.value}
       </button>
@@ -25,8 +23,15 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null);
+    };
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value={this.state.squares[i]} />;
   }
 
   render() {
